fa4fcc78d64955109f8f8b54f1fd9d9bddcbfd66
nakazawashion <nakazawa.shion@celsys.com>
2023-05-13 14:39:26 +0900

    タイトルの表示

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Blog;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function index()
+    {
+        $blogs = Blog::get();
+
+        return view('home', compact('blogs'));
+    }
 }
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>ブログ</title>
+    <link type="text/css" rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+
+
+@yield('content')
+
+
+</body>
+</html>
+
+
