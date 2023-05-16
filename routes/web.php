83dc732a553cd492e33c85d703267dce757f83cf
nakazawashion <nakazawa.shion@celsys.com>
2023-05-16 17:41:21 +0900

    ユーザーのログイン

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SignupController;
+use App\Http\Controllers\Mypage;
 use Illuminate\Support\Facades\Route;
 
 Route::get('',[HomeController::class, 'index']);
@@ -9,3 +10,6 @@ Route::get('blogs/{blog}',[HomeController::class, 'show'])->name('blog.show');
 
 Route::get('signup',[SignupController::class, 'index']);
 Route::post('signup',[SignupController::class, 'store']);
+
+Route::get('mypage/login', [Mypage\UserLoginController::class, 'index'])->name('login');
+Route::post('mypage/login', [Mypage\UserLoginController::class, 'login']);
