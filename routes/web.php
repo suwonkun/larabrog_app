96da2edd836ee437859a6e54ed48288ce23e32f3
nakazawashion <nakazawa.shion@celsys.com>
2023-05-20 21:37:46 +0900

    認証していればマイページを開く

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SignupController;
-use App\Http\Controllers\Mypage;
+use App\Http\Controllers\Mypage\UserLoginController;
+use App\Http\Controllers\Mypage\BlogController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('',[HomeController::class, 'index']);
@@ -11,5 +12,10 @@ Route::get('blogs/{blog}',[HomeController::class, 'show'])->name('blog.show');
 Route::get('signup',[SignupController::class, 'index']);
 Route::post('signup',[SignupController::class, 'store']);
 
-Route::get('mypage/login', [Mypage\UserLoginController::class, 'index'])->name('login');
-Route::post('mypage/login', [Mypage\UserLoginController::class, 'login']);
+Route::get('mypage/login', [UserLoginController::class, 'index'])->name('login');
+Route::post('mypage/login', [UserLoginController::class, 'login']);
+
+Route::middleware('auth')->group(function (){
+
+   Route::get('mypage', [BlogController::class, 'index']);
+});
